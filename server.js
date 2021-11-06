ff21193228570b420e219f6312d0258a98cf64e7
Dylan Shoemaker <me@dylanshoemaker.com>
2021-11-06 11:22:05 -0700

    set up files and folders

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());  
+
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
